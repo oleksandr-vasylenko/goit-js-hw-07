1d835885358d6be5876bc2b5e5603f7f3a4a7087
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 13:58:52 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const picTemplate = () => `
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+`;
+
+const refs = {
+  picList: document.querySelector("body"),
+};
+
+const render = () => {
+  const list = galleryItems.map((item) => picTemplate(item).join());
+  refs.picList.innerHTML = "";
+  refs.picList.insertAdjacentHTML("beforeend', list);
+};
