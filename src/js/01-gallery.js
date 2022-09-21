422b073d9ef7304fec6d4c6611d269cd5e83ffb4
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 18:29:02 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,28 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const picTemplate = ({ preview, original, description }) => `
 <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
-  </a>
 </div>
 `;
+
+// const picTemplate = ({ preview, original, description }) => `
+// <div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>
+// `;
 
 // console.log(picTemplate);
 
 const refs = {
   picList: document.querySelector("div.gallery"),
 };
-
-// console.log(refs.picList);
 
 const render = () => {
   const list = galleryItems.map((item) => picTemplate(item)).join("");
@@ -36,18 +43,9 @@ render();
 const onImageClick = (e) => {
   if (e.target === e.currentTarget) return;
 
-  console.log(e);
-  console.log(e.target);
-  console.log(e.currentTarget);
-
   const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-  `);
+    <img src="${bla}" width="800" height="600">
+`);
 
   instance.show();
 };
