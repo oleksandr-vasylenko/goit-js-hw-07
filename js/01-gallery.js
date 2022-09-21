8fb89323eb0f8e22c2d464c4700e423b453f97ca
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 21:41:05 +0300

    Sets up task #02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+console.log(galleryItems);
+
 const picTemplate = ({ preview, original, description }) => `
 <div class="gallery__item">
     <img
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const picTemplate = ({ preview, original, description }) => `
+<div class="gallery__item">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+</div>
+`;
+
+const refs = {
+  picList: document.querySelector("div.gallery"),
+};
+
+const render = () => {
+  const list = galleryItems.map((item) => picTemplate(item)).join("");
+
+  refs.picList.insertAdjacentHTML("beforeend", list);
+};
+
+render();
+
+const onImageClick = (e) => {
+  e.preventDefault();
+
+  if (e.target === e.currentTarget) return;
+
+  const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+};
+
+refs.picList.addEventListener("click", onImageClick);
