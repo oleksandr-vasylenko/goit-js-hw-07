e63c63b0d84bb241decab4f966414cddc6bd47a4
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 17:50:09 +0300

    Updates Task #02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+// console.log(galleryItems);
+
 const picTemplate = ({ preview, original, description }) => `
 <div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -14,15 +16,40 @@ const picTemplate = ({ preview, original, description }) => `
 </div>
 `;
 
+// console.log(picTemplate);
+
 const refs = {
-  picList: document.querySelector(".gallery"),
+  picList: document.querySelector("div.gallery"),
 };
+
+// console.log(refs.picList);
 
 const render = () => {
   const list = galleryItems.map((item) => picTemplate(item)).join("");
 
-  // refs.picList.innerHTML = "";
+  refs.picList.innerHTML = "";
   refs.picList.insertAdjacentHTML("beforeend", list);
 };
 
 render();
+
+const onImageClick = (e) => {
+  // if (e.target === e.currentTarget) return;
+
+  console.log(e);
+  console.log(e.target);
+  console.log(e.currentTarge);
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+
+  instance.show();
+};
+
+refs.picList.addEventListener("click", onImageClick);
