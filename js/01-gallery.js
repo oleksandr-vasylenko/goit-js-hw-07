598813c15f1a838c68506c55e90cabd95ef5b98b
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 21:45:27 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,12 +5,14 @@ console.log(galleryItems);
 
 const picTemplate = ({ preview, original, description }) => `
 <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
+  </a>
 </div>
 `;
 
