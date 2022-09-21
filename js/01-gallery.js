09f1fb36dbd56ef537a8d94d15688a7bce6b86e3
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-21 19:35:07 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// const picTemplate = ({ preview, original, description }) => `
-// <div class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </div>
-// `;
-
 const picTemplate = ({ preview, original, description }) => `
 <div class="gallery__item">
     <img
