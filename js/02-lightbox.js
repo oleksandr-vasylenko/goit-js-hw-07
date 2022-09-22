297b8483e4d3d1ecfb669ef583710172631ae766
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-22 21:16:14 +0300

    Updates image classes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,8 +3,8 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const picTemplate = ({ preview, original, description }) => `
-<li>
-    <a class="gallery__item" href="${original}">
+<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
     </a>
 </li>
