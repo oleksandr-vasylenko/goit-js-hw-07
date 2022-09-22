5fab20e4c3d14a5d30d7f985fd69642dd0bdc9cc
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-22 21:28:02 +0300

    Updates task #2
    - fixes the bugs
    - refactors the code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,10 +2,10 @@ import { galleryItems } from "./gallery-items.js";
 
 // Change code below this line
 
-const picTemplate = ({ preview, original, description }) => `
+const imgTemplate = ({ preview, original, description }) => `
 <li class="gallery__item">
     <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
     </a>
 </li>
 `;
@@ -15,17 +15,19 @@ const refs = {
 };
 
 const render = () => {
-  const list = galleryItems.map((item) => picTemplate(item)).join("");
+  const list = galleryItems.map((item) => imgTemplate(item)).join("");
   refs.picList.insertAdjacentHTML("beforeend", list);
 };
-
 render();
 
 const onImageClick = (e) => {
   e.preventDefault();
   if (e.target === e.currentTarget) return;
-
-  var lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
 };
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 refs.picList.addEventListener("click", onImageClick);
